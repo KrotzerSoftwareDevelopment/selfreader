a74549fb062fd0fe3ad9f56f5bc3ffd9c71fe6bd
deankrotzer <krotzersoftawre@gmail.com>
2021-03-06 15:04:28 -0800

    v2.1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Self readers</Text>
+      <Text>Self readerss</Text>
       <StatusBar style="auto" />
     </View>
   );
